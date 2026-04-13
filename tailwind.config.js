b072f144d7ae181b2543da1045c037866e7b9dfc
M-Atif-Latif <atifmian6767@gmail.com>
2026-04-13 15:42:53 +0500

    u

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,59 @@ export default {
   darkMode: 'class',
   theme: {
     extend: {
+      colors: {
+        // Professional color palette
+        primary: {
+          50: '#f0f5ff',
+          100: '#e0ebfe',
+          200: '#c5d9fd',
+          300: '#a3bdfc',
+          400: '#7c9efd',
+          500: '#5b7aff',
+          600: '#4552e6',
+          700: '#3537cc',
+          800: '#2d2eaa',
+          900: '#1f1f5c',
+          950: '#0f0f2e',
+        },
+        secondary: {
+          50: '#f8f9ff',
+          100: '#f0f2ff',
+          200: '#e3e8ff',
+          300: '#cdd5ff',
+          400: '#a3aeff',
+          500: '#8892ff',
+          600: '#6b7aff',
+          700: '#556dff',
+          800: '#3f5aff',
+          900: '#2f47cc',
+        },
+        accent: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#145231',
+        },
+        neutral: {
+          50: '#f9fafb',
+          100: '#f3f4f6',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+          950: '#030712',
+        },
+      },
       screens: {
         'xs': '475px',      // Extra small devices
         'sm': '640px',      // Small devices (phones)
